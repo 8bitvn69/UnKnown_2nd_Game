e0484fd1f31106581bc6ac4b7ffb8957840cc612
Phùng Trọng Hiếu <8bitvn69@gmail.com>
2017-02-19 10:33:47 +0700

    1st COMMIT - START PROJECT

---
+++ UnKnownProject/Source/UnKnownProject/UnKnownProjectGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "UnKnownProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNKNOWNPROJECT_API AUnKnownProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
